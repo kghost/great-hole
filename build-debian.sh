cbad2a8863fafe769b3e829022998e81f9bc2d11
Zang MingJie <zealot0630@gmail.com>
2026-06-10 20:22:46 +0800

    fix: update build script to use supported Debian distributions

---
+++ build-debian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+gbp buildpackage -S -us -uc --git-export-dir=build/debian --git-submodules
+
+for dist in trixie sid; do
+    echo "Building for $dist"
+    mkdir -p build/debian/$dist
+    pushd build/debian/$dist
+    sbuild -d $dist --build-dep-resolver=aptitude ../great-hole_0.1.10.dsc
+    popd
+done
