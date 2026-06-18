f4694cd2a23021f4311d55048cc70b2ab44ad401
Zang MingJie <zealot0630@gmail.com>
2026-06-18 21:06:56 +0800

    chore: update changelog for version 0.1.11 to include detailed tunnel state

--- build-debian.sh
+++ build-debian.sh
@@ -8,6 +8,6 @@ for dist in trixie sid; do
     echo "Building for $dist"
     mkdir -p build/debian/$dist
     pushd build/debian/$dist
-    sbuild -d $dist --build-dep-resolver=aptitude ../great-hole_0.1.10.dsc
+    sbuild -d $dist --build-dep-resolver=aptitude ../great-hole_0.1.11.dsc
     popd
 done
