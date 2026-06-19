2bc7440e24a934ada127ed955cccb13f7c6b67ce
Zang MingJie <zealot0630@gmail.com>
2026-06-19 20:05:48 +0800

    chore: update build script to parse version dynamically and add local directory to gitignore

--- build-debian.sh
+++ build-debian.sh
@@ -4,10 +4,12 @@ set -e
 
 gbp buildpackage -S -us -uc --git-export-dir=build/debian --git-submodules
 
+Version=$(dpkg-parsechangelog --show-field Version)
+
 for dist in trixie sid; do
     echo "Building for $dist"
     mkdir -p build/debian/$dist
     pushd build/debian/$dist
-    sbuild -d $dist --build-dep-resolver=aptitude ../great-hole_0.1.11.dsc
+    sbuild -d $dist --build-dep-resolver=aptitude ../great-hole_${Version}.dsc
     popd
 done
