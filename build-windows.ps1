c5ae38d28e448e169c6dc4424b409c66b8da8e52
Zang MingJie <zealot0630@gmail.com>
2026-07-25 04:41:39 +0800

    refactor: update CMake presets and build script for consistency with new naming conventions

--- build-windows.ps1
+++ build-windows.ps1
@@ -2,13 +2,13 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
 & "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64 -HostArch amd64
 
-cmake --workflow --preset windows-debug-msvc
-cmake --workflow --preset windows-debug-msvc-asan
-cmake --workflow --preset windows-release-msvc
-cmake --workflow --preset windows-release-msvc-asan
-cmake --workflow --preset windows-debug-ninja
-cmake --workflow --preset windows-release-ninja
-cmake --workflow --preset windows-debug-ninja-asan
-cmake --workflow --preset windows-release-ninja-asan
+cmake --workflow --preset windows-msvc-debug
+cmake --workflow --preset windows-msvc-debug-asan
+cmake --workflow --preset windows-msvc-release
+cmake --workflow --preset windows-msvc-release-asan
+cmake --workflow --preset windows-ninja-debug
+cmake --workflow --preset windows-ninja-release
+cmake --workflow --preset windows-ninja-debug-asan
+cmake --workflow --preset windows-ninja-release-asan
 
-New-Item -ItemType Junction -Path "build" -Target "build-debug-asan"
+New-Item -ItemType Junction -Path "build" -Target "build-windows-ninja-debug-asan"
